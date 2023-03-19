4069367a2022224f213c4b3f191ed65fae8e6242
HarpSaini <harpsainicodes@gmail.com>
2023-03-19 18:31:19 -0400

    Added the ability to add tasks

--- src/Components/InProgress.js
+++ src/Components/InProgress.js
@@ -2,17 +2,29 @@
 import './styles.css'
 import { useState } from 'react'
 
+
 const InProgress = ()=>{
 
-    const [InProgressList, setInpregressList] = useState([1,2,3,4,5])
+    const [InProgressList, setInprogressList] = useState([])
 
-// function that adds a new item to the list 
+// function that adds a new item to the list. 
    const handleAddingListItem = () =>{
-    console.log(InProgressList)
-   }
+    setInprogressList(current=>[...current,{type:"text", value: ""}] )
+    }
 
     const handleDragging = () => {
         console.log("dragged")
+   }
+
+   // this function is takes the value of the input and enters it at the correct index in the state object
+   const handleChange = (e) => {
+    e.preventDefault()
+    const index = e.target.id;
+    setInprogressList((current) => {
+      const newArr = [...current];
+      newArr[index].value = e.target.value;
+      return newArr;
+    });
    }
 
     return(
@@ -25,16 +37,22 @@ const InProgress = ()=>{
                 {
                   InProgressList.map((item,index)=>{
                     return(
-                        <li draggable="true" onDrag={()=>console.log("dragged")} key={index}> 
-                            <p>{item}</p>
+                        <li draggable="true" onDrag={handleDragging} key={index}> 
+                            <label htmlFor={item.value}></label>
+                            <input 
+                            type="text" 
+                            name={item.value} 
+                            id= {index} 
+                            value={item.value} 
+                            onChange={handleChange}/>
                         </li>
                     )
                   })  
                 }  
-
             </ul>
         </div>
     )
 }
 
 export default InProgress;
+
--- src/Components/ToDo.js
+++ src/Components/ToDo.js
@@ -1,16 +1,58 @@
+
 import './styles.css'
+import { useState } from 'react'
+
 
 const ToDo = ()=>{
+
+    const [ToDoList, setToDoList] = useState([])
+
+// function that adds a new item to the list. 
+   const handleAddingListItem = () =>{
+    setToDoList(current=>[...current,{type:"text", value: ""}] )
+    }
+
+    const handleDragging = () => {
+        console.log("dragged")
+   }
+
+   // this function is takes the value of the input and enters it at the correct index in the state object
+   const handleChange = (e) => {
+    e.preventDefault()
+    const index = e.target.id;
+    setToDoList((current) => {
+      const newArr = [...current];
+      newArr[index].value = e.target.value;
+      return newArr;
+    });
+   }
+
     return(
         <div className='bucketStyles'>
-            <h2>ToDo</h2>
+            <div className='bucketHeading'>
+                <h2>To Do</h2>
+                <button onClick={handleAddingListItem}>+</button>
+            </div>
             <ul className='listStyles'>
-                <li><p>sups</p></li>
-                <li><p>sups</p></li>
-                <li><p>sups</p></li>  
+                {
+                  ToDoList.map((item,index)=>{
+                    return(
+                        <li draggable="true" onDrag={handleDragging} key={index}> 
+                            <label htmlFor={item.value}></label>
+                            <input 
+                            type="text" 
+                            name={item.value} 
+                            id= {index} 
+                            value={item.value} 
+                            onChange={handleChange}/>
+                        </li>
+                    )
+                  })  
+                }  
             </ul>
         </div>
     )
 }
 
 export default ToDo;
+
